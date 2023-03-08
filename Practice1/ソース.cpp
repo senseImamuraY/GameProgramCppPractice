0fc820808c0b01a08c341447001c92c1a238d686
senseImamuraY <iy5035704@gmail.com>
2023-03-08 15:41:42 +0900

    check you can cast enum to int

---
+++ Practice1/ソース.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	signed char a = -1;
+	int b;
+
+	b = a;
+	printf("%08X (%d) \n", b, b);
+
+	b = (unsigned char)a;
+	printf("%08X (%d) \n", b, b);
+
+	int rows = 3; // s”
+	int cols = 4; // —ñ”
+
+	enum E{S,A,P};
+
+	cout << (int)A << endl;
+}
